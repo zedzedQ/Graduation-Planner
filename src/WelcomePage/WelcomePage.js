d8d3d9ac125d3e173fd89dbf09bc6ea43f8e50a7
zimingg <zimingg@uoregon.edu>
2018-12-16 16:07:47 -0800

    set up router 2

---
+++ src/WelcomePage/WelcomePage.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {NavLink} from "react-router-dom";
+
+
+// Welcome Page
+const WelcomePage = () => (
+
+    <div>
+        <NavLink  to="/main" >  Main page </NavLink>
+
+        {/* add more components here  */}
+            
+        content later
+
+    </div>
+
+)
+
+export default WelcomePage;
