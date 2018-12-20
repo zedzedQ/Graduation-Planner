9aca749a747a915272bca0b28134f36a1c113f2d
Adam Chen <a461112804@gmail.com>
2018-12-19 23:14:06 -0800

    update css

--- src/Common/AvailableCourses.js
+++ src/Common/AvailableCourses.js
@@ -9,12 +9,6 @@ const ColumnContainer = styled.div`
     display: flex;
     flex-direction: column;
 `;
-const ColumnList = styled.div`
-    padding: 8px;
-    background-color: ${props => (props.isDraggingOver ? 'skyblue' : 'white')};
-    flex-grow: 1;
-    min-height = 100px;
-`;
 
 // possible usage: <AvailableCourses sections={xxxx} state={this.state}>
 // sections = ["cisupper", "cislower", ...] state is data we need like findDict, etc.
@@ -22,18 +16,16 @@ export default class AvailableCourses extends React.Component {
     render() {
         return (
             <ColumnContainer>
-                    <ColumnList>
-                        {
-                            this.props.sections.map((sectionId, index) => {
-                                const section = this.props.state.columns[sectionId]; // find the section in columns
-                                const courses = section.courseIds.map(courseId => this.props.state.cisCourses[courseId]); // find the list of instances coressponding to the courseId in this section
-                                const isDropDisabled = false;
-                                // we need the column title for displaying so we pass it, which is section in our case here
-                                return <Column key={section.id} column={section} courses={courses} isDropDisabled={isDropDisabled}/>;
-                            })
-                        }
-                        {provided.placeholder}
-                    </ColumnList>
+                    {
+                        this.props.sections.map((sectionId, index) => {
+                            const section = this.props.state.columns[sectionId]; // find the section in columns
+                            const courses = section.courseIds.map(courseId => this.props.state.cisCourses[courseId]); // find the list of instances coressponding to the courseId in this section
+                            const isDropDisabled = false;
+                            // we need the column title for displaying so we pass it, which is section in our case here
+                            return <Column key={section.id} column={section} courses={courses} isDropDisabled={isDropDisabled}/>;
+                        })
+                    }
+                    {provided.placeholder}
                 )}
             </ColumnContainer>
         )
